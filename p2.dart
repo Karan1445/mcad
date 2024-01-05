41fa71c1cd0eee69b844fd25aa79b5da8ce2f2bc
Karan1445 <155767601+Karan1445@users.noreply.github.com>
2024-01-06 00:02:59 +0530

    prac5

--- p2.dart
+++ p2.dart
@@ -1,18 +1,17 @@
-import 'dart:io';
-
 void main(){
-    stdout.write("enter num1::");
-    int num1=int.parse(stdin.readLineSync()!);
-    stdout.write("enter num2::");
-    int num2=int.parse(stdin.readLineSync()!);
-
-    print("max is ${max(num1,num2)}");
-}
-int? max(int num1,int num2){
-  if(num1>num2){
-    return num1;
-  }
-  else{
-    return num2;
-  }
+  int x1=0;
+    List arr1=[1,2,3,4,5];
+    List arr2=[1,3,5,7,8];
+    List arr3=new List.filled(5, 0);
+    for(int i=0;i<arr1.length;i++){
+        for(int j=0;j<arr2.length;j++){
+          if(arr1[i]==arr2[j]){
+              arr3[x1]=(arr1[i]);
+              x1++;
+          }
+        }
+    }
+    for(int i=0;i<arr3.length;i++){
+       print(arr3[i]);
+    }
 }
