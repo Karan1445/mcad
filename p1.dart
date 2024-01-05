25a75e8556869cc8978c959c20b7eddbbf74ce76
Karan1445 <155767601+Karan1445@users.noreply.github.com>
2024-01-06 00:01:42 +0530

    prac4

--- p1.dart
+++ p1.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
 void main(){
-    stdout.write("enter a start range::");
-    int start=int.parse(stdin.readLineSync()!);
+    stdout.write("enter amount::");
+    int p=int.parse(stdin.readLineSync()!);
+    stdout.write("enter interest::");
+    double i=double.parse(stdin.readLineSync()!);
+    stdout.write("enter year::");
+    int y=int.parse(stdin.readLineSync()!);
 
-    stdout.write("enter a end range::");
-    int end=int.parse(stdin.readLineSync()!);
+    print("interser is ${cal(p,i,y)}");
+}
 
-    for(int i=start;i<end;i++){
-      if(i%2==0 && !(i%3==0)){
-        print(i);
-      }
-    }
+double? cal(int p,double i,int y){
+  return (((p*i*y)/100));
 }
--- p5.dart
+++ p5.dart
@@ -1,13 +1,24 @@
+
 import 'dart:io';
 
 void main(){
-    stdout.write("enter a string::");
-    String x=(stdin.readLineSync()!);
+  stdout.write("enter size of array::");
+  int n=int.parse(stdin.readLineSync()!);
+  List arr=new List.filled(n,null);
 
-    int len=x.split('').length;
-    final arr=x.split('');
+  for(int i=0;i<n;i++){
+    arr[i]=(int.parse(stdin.readLineSync()!));
+  }
 
-    for(int i=(len-1);i>=0;i--){
-      stdout.write(arr[i]);
+  int eve=0,odd=0;
+
+  for(int i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+      eve++;
+    }else if(arr[i]%2==1){
+      odd++;
     }
+  }
+
+  print("total odd number is $odd and even is $eve");
 }
